d2338aa827dcff0adc76725c0c1d4f8189cd0ddb
Fred <fduartej@gmail.com>
2018-10-16 23:51:20 -0500

    Validation

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,10 +1,14 @@
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace demomvc.Models
 {
     public class Student
     {
-        public string ID { get; set; }
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Please enter last name")]
+		[StringLength(40)]
         public string Name { get; set; }
     }
 
