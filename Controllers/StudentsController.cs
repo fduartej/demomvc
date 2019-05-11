081979da5d758cdef2c7a8f15fe4b90a7c47a77b
Fred <fduartej@gmail.com>
2019-05-11 11:57:52 -0500

    Cambios de Validacion

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -33,7 +33,7 @@ namespace demomvc.Controllers
         // POST: Students/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name")] Student student)
+        public async Task<IActionResult> Create([Bind("Name")] Student student)
         {
             if (ModelState.IsValid)
             {
