b46facf536e9b066f3e4522a28c6d77121d42c04
Fred <fduartej@gmail.com>
2019-05-11 12:20:26 -0500

    Adding Changes LastName

---
+++ Migrations/20190511171718_LastName.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace demomvc.Migrations
+{
+    public partial class LastName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Student",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Student",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Student");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Student",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+        }
+    }
+}
